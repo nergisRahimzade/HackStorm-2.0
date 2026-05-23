69e1c866a69d8071446ae376d148e5489ee26dad
josue <jcruz323@mail.ccsf.edu>
2026-05-23 12:24:14 -0700

    fix: decreased delays and sequence of alert targetted toward high alert

--- smartCaneSensor/smartCaneSensor.ino
+++ smartCaneSensor/smartCaneSensor.ino
@@ -12,23 +12,32 @@
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 long a;
 
-// notes in the melody:
-int melody[] = {
-  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
-int duration = 500;  // 500 miliseconds
+// selected pitches.h sound fitted toward alerting about a real time alert of an object nearby
+int alertMelody[] = {
+  NOTE_C6, NOTE_G6, NOTE_C6, NOTE_G6,
+  NOTE_A6, NOTE_G6, NOTE_A6, NOTE_G6,
+  NOTE_C7, NOTE_C7, NOTE_G6
+};
+
+int alertDurations[] = {
+  150, 150, 150, 150,
+  120, 120, 120, 120,
+  200, 200, 400
+};
+
+int lenAlertDuration = sizeof(alertDurations) / sizeof(alertDurations[0]);
  
 void setup() {
    Serial.begin(9600);
-   delay(1000);
 }
 
 void alertSound(){
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(8, melody[thisNote], duration);
-     
-    // Output the voice after several minutes
-    delay(1000);
+  // alert sound focus: on grabbing user's attention as quick as possible
+  for(int startPitch = 0; startPitch < lenAlertDuration; ++startPitch){
+    tone(8, alertMelody[startPitch], alertDurations[startPitch]);
+    delay(alertDurations[startPitch] + 50); 
+
+    delay(20);
   }
 }
 
@@ -39,13 +48,13 @@ void loop() {
    a=sr04.Distance();
    Serial.print(a);
    Serial.println("cm");
-   delay(1000);
+   delay(200);
 
    // if object detection is 5cm away alert the user of an object 
-   if(a == 5){
+   if(a <= 10){
     alertSound();
    }
 
   // restart after two seconds 
-  delay(2000);
+  delay(200);
 }
